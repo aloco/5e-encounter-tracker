c0dff9dab4ec919e0ec27c1d7546d6ba62914606
Andreas Tinoco Lobo <code@tinoco-lobo.me>
2018-09-22 23:34:53 +0200

    adds footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './App.css';
 import InitiativeTracker from './components/InitiativeTracker';
+import { Container, Row, Col } from 'reactstrap';
+
 
 // declare icons
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -11,13 +13,24 @@ library.add(faClone, faTrash, faChevronCircleLeft, faChevronCircleRight, faPlusC
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">a very basic 5e initiative tracker</h1>
-        </header>
-        <section>
-          <InitiativeTracker/>
-        </section>
+      <div className="app-wrapper">
+        <div className="App">
+          <header className="App-header bg-dark">
+            <h1 className="App-title">a very basic 5e initiative tracker</h1>
+          </header>
+          <section className="app-section">
+            <InitiativeTracker/>
+          </section>
+        </div>
+        <footer className="text-light bg-dark">
+          <Container>
+            <Row>
+              <Col>
+                Made without dices but with love | © 2018 Andreas Tinoco Lobo | MIT | <a href="https://github.com/aloco/5e-encounter-tracker" className="text-light">See project on Github</a>
+              </Col>
+            </Row>
+          </Container>
+        </footer>
       </div>
     );
   }
