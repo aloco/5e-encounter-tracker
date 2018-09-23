0e68c6850d1f4895155463314b00948cef6b7fa6
Andreas Tinoco Lobo <code@tinoco-lobo.me>
2018-09-23 12:50:52 +0200

    fixed layout issue with footer

--- src/App.tsx
+++ src/App.tsx
@@ -13,25 +13,25 @@ library.add(faClone, faTrash, faChevronCircleLeft, faChevronCircleRight, faPlusC
 class App extends React.Component {
   public render() {
     return (
-      <div className="app-wrapper">
         <div className="App">
           <header className="App-header bg-dark">
             <h1 className="App-title">a very basic 5e initiative tracker</h1>
           </header>
-          <section className="app-section">
-            <InitiativeTracker/>
-          </section>
+          <main>
+            <section className="app-section">
+              <InitiativeTracker/>
+            </section>
+          </main>
+          <footer className="text-light bg-dark">
+            <Container>
+              <Row>
+                <Col>
+                  Made with love but without dice | © 2018 Andreas Tinoco Lobo | MIT | <a href="https://github.com/aloco/5e-encounter-tracker" className="text-light">See project on Github</a>
+                </Col>
+              </Row>
+            </Container>
+          </footer>
         </div>
-        <footer className="text-light bg-dark">
-          <Container>
-            <Row>
-              <Col>
-                Made without dices but with love | © 2018 Andreas Tinoco Lobo | MIT | <a href="https://github.com/aloco/5e-encounter-tracker" className="text-light">See project on Github</a>
-              </Col>
-            </Row>
-          </Container>
-        </footer>
-      </div>
     );
   }
 }
